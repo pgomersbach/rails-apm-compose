b7fb14a5f41aa6837ccdb2918ddac45a228c5264
Paul Gomersbach <paul.gomersbach@mn.nl>
2019-09-12 15:10:39 +0000

    add apache container

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 docker-compose run --rm web rails g model Joke body:text
 docker-compose run --rm web rails db:create
 docker-compose run --rm web rails db:migrate
-docker-compose run --rm web rails runner 'Joke.create(body: "Knock! Knock! Whos there? Owls say. Owls say who? Yes, they do.")'
-docker-compose run --rm web rails runner 'Joke.create(body: "Knomt een man bij de ..")'
+docker-compose run --rm web rails runner 'Joke.create(body: "Sphinx of black quartz, judge my vow.")'
+docker-compose run --rm web rails runner 'Joke.create(body: "The five boxing wizards jump quickly")'
 
